6f03f61601f8278e28dd0ae776f9588f8e66adaf
Hamid <hamid.iqbal2001@gmail.com>
2022-10-13 19:13:34 +0100

    Spilt app componets into seperate .js files

--- App.js
+++ App.js
@@ -1,50 +1,36 @@
 // This demo is to focus on iOS and Android development with React Native.
 // Testing devices used were an iPhone 13 running iOS 16.0 and Pixel 6 running Android 13
 
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import { useState } from 'react'
+
+// Import of separate components 
+import GoalItem from './components/goalItem';
+import GoalInput from './components/goalInput';
 
 export default function App() {
 
-  const [enteredGoalText, setEnteredGoalText] = useState('');
-  const [courseGoals, setCourseGoals] = useState([]);
-
-
-  // Function to handle the input value provided in the field on screen, passes to addGoalHandler
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText)
-    console.log(enteredText)
-    
-  };
-
+const [courseGoals, setCourseGoals] = useState([]);
 
 // Function that returns an object enteredGoalText with a key, that is assigned a random value/digit by the Math Module
-  function addGoalHandler(){
+  function addGoalHandler(enteredGoalText){
     console.log(enteredGoalText)
     setCourseGoals(currentCourseGoals => [...currentCourseGoals, {text: enteredGoalText, key: Math.random().toString()}])
   };
-
-// Returning the 
+ 
   return (
     <View style={styles.appContainer}>
-    <View style={styles.inputContainer}>
-      <TextInput placeholder='Your main goal' style={styles.textInput} onChangeText={goalInputHandler}/>
-      <Button title='Add Goal' onPress={addGoalHandler}/>
-    </View>
-
+{/* PROP FOR INPUT HERE */}
+    <GoalInput onAddGoal={addGoalHandler}/>
 
 {/* 
   https://reactnative.dev/docs/flatlist
-  Using FlatList to improve applications perfoamce compared to ScrollView which negatively impacts applications performance with large dynamic arrays of data
+  Using FlatList to improve applications performance compared to ScrollView which negatively impacts applications performance with large dynamic arrays of data
 */}
     <View style={styles.goalsContainer}>
-    <FlatList data={courseGoals} renderItem={(itemData) => {
-      return (
-        <View style={styles.goalItem}>
-          <Text style={styles.goalItem}>{itemData.item.text}</Text>
-        </View>
-      );
-    }} alwaysBounceVertical={false} />
+    <FlatList alwaysBounceVertical={false} data={courseGoals} renderItem={(itemData) => {
+      return <GoalItem text={itemData.item.text} />;
+    }}  />
     </View>
     </View>
   );
@@ -57,30 +43,9 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%', 
-    margin: 8,
-    padding: 8,
-  },
+
   goalsContainer: {
     flex: 5,
   },
-  goalItem: {
-    margin: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-    padding: 8,
-    color: 'white'
-  }
+
 });
---
+++ components/goalItem.js
@@ -0,0 +1,26 @@
+import {StyleSheet, View, Text} from 'react-native';
+
+function GoalItem(props) {
+    return (
+    <View style={styles.goalItem}>
+        <Text style={styles.goalItem}>{props.text}</Text>
+    </View>
+    ) 
+
+};
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+    goalItem: {
+        margin: 8,
+        borderRadius: 6,
+        backgroundColor: '#5e0acc',
+        padding: 8,
+        color: 'white'
+      },
+
+      goalText: {
+        color: 'white',
+      }
+});
